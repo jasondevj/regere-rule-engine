3d29491abb795060e6382153f094fe80ea791c05
Jason <jasondevj@gmail.com>
2012-09-23 12:46:10 +0800

    copyright statement updated

--- src/test/java/dev/j/regere/parser/json/DefaultJsonParserTest.java
+++ src/test/java/dev/j/regere/parser/json/DefaultJsonParserTest.java
@@ -1,15 +1,19 @@
 /*
- *   (C) Copyright 2012-2013 hSenid Software International (Pvt) Limited.
- *   All Rights Reserved.
+ * Copyright 2012 The regere-rule-engine Project
  *
- *   These materials are unpublished, proprietary, confidential source code of
- *   hSenid Software International (Pvt) Limited and constitute a TRADE SECRET
- *   of hSenid Software International (Pvt) Limited.
+ * The regere-rule-engine Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
  *
- *   hSenid Software International (Pvt) Limited retains all title to and intellectual
- *   property rights in these materials.
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
  */
+
 package dev.j.regere.parser.json;
 
 import org.junit.Test;
--- src/test/java/dev/j/regere/service/Test.java
+++ src/test/java/dev/j/regere/service/Test.java
@@ -14,19 +14,12 @@
  * under the License.
  */
 
-package dev.j.regere.respository;
+package dev.j.regere.service;
 
-import dev.j.regere.domain.RegereRuleFlowWrapper;
-
-import java.util.List;
-import java.util.Map;
-
-public interface IntermediatePersistedTable {
-
-    void init();
-
-    RegereRuleFlowWrapper load(String regereId, String commonIdentifier, Map<String, Object> currentEvent);
-
-    void persistEvent(String regereId, String commonIdentifier, RegereRuleFlowWrapper flowWrapper,
-                      List<String> persitableKeys);
+/**
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $LastChangedRevision$
+ */
+public class Test {
 }
